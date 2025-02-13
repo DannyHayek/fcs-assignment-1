dfeebe78e0884e073e0b86f9a403acf37ed9cc28
Danny <dhayek92@gmail.com>
2025-02-13 10:29:33 +0200

    Added python file for exercise 2

---
+++ Exercise2.py
@@ -0,0 +1,6 @@
+number = input("Enter a number: ")
+
+if int(number) % 2 == 0:
+    print("The number is even!")        #Not sure if we are allowed to use the remainder operator, but this was simplest solution I knew to use
+else:
+    print("The number is odd!")
